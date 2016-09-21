1aa6c5191234810ea1d15c09328c74436182207e
Husiv Maksym <maksym.husiv@musicqubed.com>
2016-09-21 13:33:45 +0300

    commit

---
+++ src/swing/layout/Test.java
@@ -0,0 +1,11 @@
+package swing.layout;
+
+import javax.swing.*;
+
+/**
+ * Created by husiv on 9/21/16.
+ */
+public class Test {
+    private JTextField textField1;
+    private JTextField textField2;
+}
