0d6b0c67c8d5ff6a2664fed1e965a898c7c3a6a9
Husiv Maksym <maksym.husiv@musicqubed.com>
2016-10-04 13:11:52 +0300

    commit

---
+++ src/swing/web/PreferencesLesson.java
@@ -0,0 +1,21 @@
+package swing.web;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+/**
+ * Created by husiv on 10/4/16.
+ */
+public class PreferencesLesson {
+    public static void main(String[] args) throws IOException, BackingStoreException {
+        Preferences root = Preferences.userRoot();
+        Preferences systemRoot = Preferences.systemRoot();
+        Preferences node = root.node("/com/max/lessons");
+        Preferences node2 = root.userNodeForPackage(new PreferencesLesson().getClass());
+        node.put("myInt", "2");
+        System.out.println(node.getInt("myInt", 1));
+        root.exportNode(new FileOutputStream("prop.xml"));
+    }
+}
