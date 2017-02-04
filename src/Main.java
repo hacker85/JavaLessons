634a17b441be84b927535b50c13c9b3b5684adbc
max <hacker_@list.ru>
2017-02-04 19:27:02 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import org.xml.sax.SAXException;
 
+import javax.mail.MessagingException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerException;
@@ -7,7 +8,7 @@ import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XMLStreamException, TransformerException, XPathExpressionException {
-
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XMLStreamException, TransformerException, XPathExpressionException, MessagingException {
+        
     }
 }
