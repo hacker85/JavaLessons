406cbb0bfc6908dd274ccc51bc67bcd0f80df046
max <hacker_@list.ru>
2017-02-18 02:39:23 +0200

    test

--- src/security/RSALesson.java
+++ src/security/RSALesson.java
@@ -4,24 +4,25 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.*;
 
 /**
- * Created by max on 2/17/17.
+ * Created by max on 2/18/17.
  */
-public class CipherLesson {
+public class RSALesson {
     public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, UnsupportedEncodingException {
-        Cipher cipher = Cipher.getInstance("AES");
+        Cipher cipher = Cipher.getInstance("RSA");
         int mode = Cipher.ENCRYPT_MODE;
 
-//        KeyGenerator kgen = KeyGenerator.getInstance("AES");
-//        kgen.init(128);
-//        SecretKey key = kgen.generateKey();
+        KeyPairGenerator pairgen = KeyPairGenerator.getInstance("RSA");
+//        SecureRandom random = new SecureRandom();
+//        pairgen.initialize(512, random);
+        KeyPair keyPair = pairgen.generateKeyPair();
+        Key publicKey = keyPair.getPublic();
+        Key privateKey = keyPair.getPrivate();
 
-        SecretKeySpec key = new SecretKeySpec("Bar12345Bar12345".getBytes(), "AES");
-        cipher.init(mode, key);
+        cipher.init(mode, privateKey);
 
         String hello = "hello world";
         byte[] bytes = cipher.doFinal(hello.getBytes(), 0, hello.length());
@@ -30,13 +31,11 @@ public class CipherLesson {
         }
         System.out.println("\n----------");
 
-        Cipher decryptCipher = Cipher.getInstance("AES");
-        decryptCipher.init(Cipher.DECRYPT_MODE, key);
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, publicKey);
         byte[] bytes2 = decryptCipher.doFinal(bytes, 0, bytes.length);
         for(byte b : bytes2) {
             System.out.print((char)b);
         }
-//        CipherInputStream inputStream;
-//        CipherOutputStream outputStream;
     }
 }
