20abeb3368914f7ff24203a51fd7c027b5afc806
max <hacker_@list.ru>
2017-03-24 11:52:30 +0200

    test

--- src/regexp/IfThenElseLesson.java
+++ src/regexp/IfThenElseLesson.java
@@ -1,8 +1,5 @@
 package regexp;
 
-import jregex.Matcher;
-import jregex.Pattern;
-
 public class IfThenElseLesson {
     public static void main(String[] args) {
         //need jRegex
@@ -11,15 +8,15 @@ public class IfThenElseLesson {
         //(?(?=regex)then|else)
         ///(?(?=condition)(then1|then2|then3)|(else1|else2|else3))
         //System.out.println("bd".matches("(a)?b(?(1)c|d)"));
-        Pattern p = new Pattern("(a)?b(?(1)c|d)");//^
-//        Pattern p = new Pattern("(?<test>a)?b(?(\\k<test>)c|d)");//not work
-//        Matcher m=p.matcher("bd");
-//        Matcher m=p.matcher("abc");
-//        Matcher m=p.matcher("bc");
-        Matcher m=p.matcher("abd");
-        while(m.find()) {
-            System.out.print(m.start() + " " + m + " ");
-        }
-        System.out.println("");
+//        Pattern p = new Pattern("(a)?b(?(1)c|d)");//^
+////        Pattern p = new Pattern("(?<test>a)?b(?(\\k<test>)c|d)");//not work
+////        Matcher m=p.matcher("bd");
+////        Matcher m=p.matcher("abc");
+////        Matcher m=p.matcher("bc");
+//        Matcher m=p.matcher("abd");
+//        while(m.find()) {
+//            System.out.print(m.start() + " " + m + " ");
+//        }
+//        System.out.println("");
     }
 }
