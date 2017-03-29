fa528f582b0ad5c1fa92283e674184b43de55454
max <hacker_@list.ru>
2017-03-29 16:01:42 +0300

    test

--- ee/src/servlets/SessionServlet.java
+++ ee/src/servlets/SessionServlet.java
@@ -5,12 +5,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet("/session")
 public class SessionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        HttpSession session = req.getSession();
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while(attributeNames.hasMoreElements()) {
+            String attrName = attributeNames.nextElement();
+            System.out.println(attrName + " = " + session.getAttribute(attrName));
+        }
+        session.setAttribute("one", "two");
+//        session.removeAttribute("one");
+//        session.invalidate();
+        System.out.println(session.getMaxInactiveInterval());
     }
 }
