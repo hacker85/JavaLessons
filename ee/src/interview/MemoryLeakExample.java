18cc7de36d4d4d6b95630c96a6a3d489a8f94662
max <hacker_@list.ru>
2017-06-20 21:35:56 +0300

    test

---
+++ ee/src/interview/MemoryLeakExample.java
@@ -0,0 +1,37 @@
+package interview;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class MemoryLeakExample {
+    static GoodCache goodCache = new GoodCache();
+    static BadCache badCache = new BadCache();
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 100_000_000; i++) {
+//            Thread.sleep(10);
+            addGoodCache(i);
+            addBadCache(i);
+        }
+    }
+    private static void addGoodCache(int i) {
+        goodCache.add(i);
+    }
+    private static void addBadCache(int i) {
+        badCache.add(i);
+    }
+}
+
+class GoodCache {
+    Map<Integer, Integer> weakMap = new WeakHashMap<>();
+    public void add(int i) {
+        weakMap.put(i, i);
+    }
+}
+class BadCache {
+    Map<Integer, Integer> hashedMap = new HashedMap();
+    public void add(int i) {
+        hashedMap.put(i, i);
+    }
+}
